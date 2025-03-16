83eb4426db377feec20ca277d48d05328edcaec6
NKGUPTA26 <niraj1998gupta@gmail.com>
2025-03-17 00:42:00 +0530

    fixing corps error

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
-
+from flask_cors import CORS
 app = create_app()
+
+# Allow all origins (for testing)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
+    # app.run(host="127.0.0.1",port=5000,debug=True)
